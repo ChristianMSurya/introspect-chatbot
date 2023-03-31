31e531ae557744f621dd79c853bdd314f54d69d1
Christian Matthew <87283634+ChristianMSurya@users.noreply.github.com>
2023-03-31 06:33:11 -0700

    Simplified everything

--- app.py
+++ app.py
@@ -1,133 +1,52 @@
+import os
+import csv
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
-from dotenv import load_dotenv
-import random
+from twilio.rest import Client
 import schedule
 import time
 import threading
-import os
-import json
-import threading
-import csv
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-DAILY_DILEMMAS_TIME = os.getenv('DAILY_DILEMMAS_TIME', '08:00')
+DAILY_DILEMMAS_TIME = os.getenv('DAILY_DILEMMAS_TIME', '06:25')
 load_dotenv()
 
-# Example dilemmas and historical outcomes
-dilemmas = [
-    {
-        "dilemma": "Dilemma 1: Description",
-        "outcome": "Outcome 1: Historical resolution"
-    },
-    {
-        "dilemma": "Dilemma 2: Description",
-        "outcome": "Outcome 2: Historical resolution"
-    },
-    # Add more dilemmas here
-]
+# Replace these with your desired dilemma and outcome
+dilemma = "Introspect 1: Imagine that you and your friends created a very popular product that revolutionized its market, generating over $1B per year. However, it was later discovered that it had unintended negative health consequences. What would you do? Would you keep selling or halt production? How would you communicate with the public?"
+outcome = "Today's dilemma is based on the company called Juul. In 2015, Juul was founded by a group of friends who created an e-cigarette that quickly gained widespread popularity, generating over $1 billion in annual revenue. However, it was later revealed that the product had negative health consequences such as addiction and respiratory problems. Despite this knowledge, Juul continued to aggressively market its product to young people, including those who had never smoked before. As a result, a surge in e-cigarette use among youth led to concerns about a potential public health crisis. In response, Juul faced significant legal and regulatory challenges and was accused of contributing to a new generation of nicotine addiction."
+feedback = "Read more about Juul here: https://www.theguardian.com/us-news/2022/sep/06/juul-teen-vaping-settlement-us-states Also, we'd love to hear feedback! What do you like about Introspect? What do you not like? What would you like to see? Feel free to reach out to us at cmatthe3@nd.edu or 302-333-2703. Until next time!"
 
-# Map dilemma descriptions to their outcomes
-dilemma_outcomes = {dilemma['dilemma']: dilemma['outcome'] for dilemma in dilemmas}
+# Your Twilio credentials
+account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+twilio_phone_number = "+18447551091"
 
+def read_phone_numbers(file_name):
+    with open(file_name, 'r', encoding='utf-8') as file:
+        csv_reader = csv.reader(file)
+        return [row[0] for row in csv_reader]
 
+# Read phone numbers from the CSV file
+phone_numbers_file = 'phone_numbers.csv'
+phone_numbers = read_phone_numbers(phone_numbers_file)
 
-
-# Read the phone numbers from the responses.csv file
-with open('responses.csv', 'r') as f:
-    reader = csv.reader(f)
-    next(reader) # skip header row
-    phone_numbers_responses = set([row[0] for row in reader])
-
-
-
-def send_intro(phone_number):
-    from twilio.rest import Client
-
-    # Your Twilio credentials
-    account_sid = os.getenv('TWILIO_ACCOUNT_SID')
-    auth_token = os.getenv('TWILIO_AUTH_TOKEN')
-    twilio_phone_number = "+18447551091"
-
-    client = Client(account_sid, auth_token)
-
-    # Send the intro message to the user
-    client.messages.create(
-        body="Thanks for signing up to the first MVP of the Introspect Chatbot. At 8 AM PST every day, Introspect will send you a philosophical dilemma. You'll be asked to reply with what you would do. After you reply, Introspect will send you more information about what ended up happening. We will store your answer so that you can come back to them in the future. We hope that this exercise will help you introspect about your values and encourage you to do good.",
-        from_=twilio_phone_number,
-        to=phone_number
-    )
-
-    # Select the first dilemma in the list
-    dilemma = dilemmas[0]
-
-    # Save the user's current dilemma
-    with open('responses.csv', 'a', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow([phone_number, dilemma["dilemma"], ""])
-
-    # Send the first dilemma to the user
-    send_dilemma(phone_number)
 
 def send_dilemma(phone_number):
-    from twilio.rest import Client
-
-    # Your Twilio credentials
-    account_sid = os.getenv('TWILIO_ACCOUNT_SID')
-    auth_token = os.getenv('TWILIO_AUTH_TOKEN')
-    twilio_phone_number = "+18447551091"
-
     client = Client(account_sid, auth_token)
-
-    # Check if this is a new user
-    current_dilemma_index = 0
-    with open('responses.csv', 'r') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            if row[0] == phone_number:
-                current_dilemma_index += 1
-
-    # Select the next dilemma in the list
-    dilemma = dilemmas[current_dilemma_index]
-
-    # Save the user's current dilemma
-    with open('responses.csv', 'a', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow([phone_number, dilemma["dilemma"], ""])
-
-    # Send the dilemma to the user
+    time.sleep(1)
     client.messages.create(
-        body=dilemma["dilemma"],
+        body=dilemma,
         from_=twilio_phone_number,
         to=phone_number
     )
 
 def daily_dilemmas():
-    # Read the phone numbers from the phone_numbers.csv file
-    with open('phone_numbers.csv', 'r') as f:
-        reader = csv.reader(f)
-        next(reader) # skip header row
-        phone_numbers = set([row[0] for row in reader])
+    for phone_number in phone_numbers:
+        send_dilemma(phone_number)
 
-    # Read the phone numbers from the current_users.csv file
-    with open('current_users.csv', 'r') as f:
-        reader = csv.reader(f)
-        next(reader) # skip header row
-        current_users = set([row[0] for row in reader])
-
-    # Find the phone numbers that are in the phone_numbers.csv file but not in the current_users.csv file
-    phone_numbers_to_add = phone_numbers.difference(current_users)
-
-    # Add new phone numbers to the current_users.csv file
-    with open('current_users.csv', 'a', newline='') as f:
-        writer = csv.writer(f)
-        for phone_number in phone_numbers_to_add:
-            writer.writerow([phone_number])
-
-
-# Schedule daily dilemmas
 schedule.every().day.at(DAILY_DILEMMAS_TIME).do(daily_dilemmas)
 
 def run_schedule():
@@ -135,119 +54,29 @@ def run_schedule():
         schedule.run_pending()
         time.sleep(1)
 
-        # Read the phone numbers from the phone_numbers.csv file
-        with open('phone_numbers.csv', 'r') as f:
-            reader = csv.reader(f)
-            phone_numbers_csv = set([row[0] for row in reader])
-
-        # Read the phone numbers from the current_users.csv file
-        with open('current_users.csv', 'r') as f:
-            reader = csv.reader(f)
-            next(reader) # skip header row
-            current_users = set([row[0] for row in reader])
-
-        # Find the phone numbers that are in the phone_numbers.csv file but not in the current_users.csv file
-        phone_numbers_to_add = phone_numbers_csv.difference(current_users)
-
-        # Send the intro message to new phone numbers
-        for phone_number in phone_numbers_to_add:
-            # Check if the phone number already exists in current_users.csv
-            with open('current_users.csv', 'r') as f:
-                reader = csv.reader(f)
-                if phone_number not in [row[0] for row in reader]:
-                    # Phone number is new, send the intro message
-                    send_intro(phone_number)
-
-                    # Add the phone number to the current_users.csv file
-                    with open('current_users.csv', 'a', newline='') as f:
-                        writer = csv.writer(f)
-                        writer.writerow([phone_number])
-
-        # Call daily_dilemmas to update current_users.csv
-        daily_dilemmas()
-
-
-
-def send_daily_dilemma(phone_number):
-    from twilio.rest import Client
-
-    # Your Twilio credentials
-    account_sid = os.getenv('TWILIO_ACCOUNT_SID')
-    auth_token = os.getenv('TWILIO_AUTH_TOKEN')
-    twilio_phone_number = "+18447551091"
-
-    client = Client(account_sid, auth_token)
-
-    # Select a random dilemma from the list
-    dilemma = random.choice(dilemmas)
-
-    # Save the user's current dilemma
-    with open('responses.csv', 'a', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow([phone_number, dilemma["dilemma"], ""])
-
-    # Send the dilemma to the user
-    client.messages.create(
-        body=dilemma["dilemma"],
-        from_=twilio_phone_number,
-        to=phone_number
-    )
-
-
-
-# Start the scheduler in a separate thread
 scheduler_thread = threading.Thread(target=run_schedule)
 scheduler_thread.start()
 
-# Load the phone numbers from the responses.csv file into memory
-with open('responses.csv', 'r') as f:
-    reader = csv.reader(f)
-    next(reader) # skip header row
-    phone_numbers_responses = set([row[0] for row in reader])
-
-@app.route("/sms", methods=["POST"])
 @app.route("/sms", methods=["POST"])
 def sms_reply():
     phone_number = request.form["From"]
     message_body = request.form["Body"]
 
-    # Get the user's current dilemma
-    with open('responses.csv', 'r') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            if row[0] == phone_number:
-                current_dilemma = row[1]
-                current_response = row[2]
+    resp = MessagingResponse()
 
-    # Get the outcome for the user's current dilemma
-    dilemma_outcome = dilemma_outcomes.get(current_dilemma)
-
-    if current_dilemma and not current_response:
-        # The user has received a new dilemma, now send it to them
-        resp = MessagingResponse()
-        resp.message(current_dilemma)
-    elif current_dilemma and current_response:
-        # The user has sent their response, now send the outcome
-        resp = MessagingResponse()
-        resp.message(dilemma_outcome)
-        # Update the user's response in the responses.csv file
-        with open('responses.csv', 'r') as f:
-            reader = csv.reader(f)
-            rows = list(reader)
-        with open('responses.csv', 'w', newline='') as f:
-            writer = csv.writer(f)
-            writer.writerows(rows)
-            for row in rows:
-                if row[0] == phone_number and row[1] == current_dilemma:
-                    row[2] = current_response
-            writer.writerows(rows)
+    if message_body.strip().lower() == "done":
+        resp.message(outcome)
+        time.sleep(2)
+        resp.message(feedback)
     else:
-        # The user has not received a dilemma yet, send the intro message
-        resp = MessagingResponse()
-        resp.message("Thanks for signing up to the first MVP of the Introspect Chatbot. At 8 AM PT every day, Introspect will send you a philosophical dilemma. You'll be asked to reply with what you would do. After you reply, Introspect will send you more information about what ended up happening. We hope that this exercise will help you introspect about your values and encourage you to do good.")
+        resp.message("Message received. Send 'done' to finish and continue.")
+
+        # Update the user's response in the responses.csv file
+        with open('responses.csv', 'a', newline='', encoding='utf-8') as f:
+            writer = csv.writer(f, lineterminator='\n')
+            writer.writerow([phone_number, dilemma, message_body.strip()])
 
     return str(resp)
-
 
 if __name__ == "__main__":
     app.run(debug=True, port=5009)
---
+++ intro.py
@@ -0,0 +1,43 @@
+import os
+import csv
+from twilio.rest import Client
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Your Twilio credentials
+account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+twilio_phone_number = "+18447551091"
+
+def read_phone_numbers(file_name):
+    with open(file_name, 'r', encoding='utf-8') as file:
+        csv_reader = csv.reader(file)
+        return [row[0] for row in csv_reader]
+
+# Read phone numbers from the CSV file
+phone_numbers_file = 'phone_numbers.csv'
+phone_numbers = read_phone_numbers(phone_numbers_file)
+
+# Your introduction message
+intro_message = (
+    "Hey, this is Christian, Dom, Jose, and Santiago. Thanks for signing up to the first MVP of the Introspect Chatbot. At 10 AM PST every day, Introspect will send you a philosophical dilemma. "
+    "Reply with what you would do as a daily exercise for yourself. When finished, type 'done' at the next message. "
+    "We will store your answers so that you can come back to them in the future. "
+    "We hope that this exercise will help you introspect about your values and encourage you to do good in the world."
+)
+
+def send_intro_message(phone_number):
+    client = Client(account_sid, auth_token)
+    client.messages.create(
+        body=intro_message,
+        from_=twilio_phone_number,
+        to=phone_number
+    )
+
+def send_intro_messages():
+    for phone_number in phone_numbers:
+        send_intro_message(phone_number)
+
+if __name__ == "__main__":
+    send_intro_messages()
